20e10ab8ee79470f13f08139ca1d26963fe116a5
Srimanyuacharyah <srimanyuacharya@gmail.com>
2026-02-07 12:24:45 +0530

    commit

--- db.py
+++ db.py
@@ -9,11 +9,11 @@ Base = declarative_base()
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 print("DATABASE_URL",DATABASE_URL)
-engine = create_engine(DATABASE_URL,connect_args={"check_same_thread":False})
+engine = create_engine(DATABASE_URL)
 sessionlocal=sessionmaker(autocommit=False,autoflush=False,bind=engine)
 
 def get_db():
-    db = create_engine(DATABASE_URL)
+    db = sessionlocal()
     try:
         yield db
     finally:
