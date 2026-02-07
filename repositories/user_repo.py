474ff9be54dc25c3484842590eae242f55b18fd1
Srimanyuacharyah <srimanyuacharya@gmail.com>
2026-02-07 14:25:44 +0530

    commit

--- repositories/user_repo.py
+++ repositories/user_repo.py
@@ -10,3 +10,6 @@ class UserRepo:
         self.db.add(user)
         self.db.commit()
         return user
+    
+    def get_user_by_email(self, email: str):
+        return self.db.query(User).filter(User.email == email).first()
